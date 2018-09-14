7e64a81fd40bf65a94f0e72789afe53e2b125e73
Allan Huang <allan.huang@recreo.com.au>
2018-09-14 10:45:56 +1000

    init

---
+++ MaintainApi/Controllers/MarketOfferController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaintainApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarketOfferController : ControllerBase
+    {
+        // GET: api/MarketOffer
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/MarketOffer/5
+        [HttpGet("{LegalEntityId}", Name = "Get")]
+        public string Get(int LegalEntityId)
+        {
+            return $"value={LegalEntityId}";
+        }
+
+        // POST: api/MarketOffer
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/MarketOffer/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
